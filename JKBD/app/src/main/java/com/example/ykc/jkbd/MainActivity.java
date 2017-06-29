a1b11fef734329ab9a70ca1cde8dbbf1c5e906de
1246015067 <1246015067@qq.com>
2017-06-29 23:54:03 +0800

    获取网络数据

--- JKBD/app/src/main/java/com/example/ykc/jkbd/MainActivity.java
+++ JKBD/app/src/main/java/com/example/ykc/jkbd/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.ykc.jkbd;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.example.ykc.jkbd.bean.information;
+import com.example.ykc.jkbd.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +22,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void text(View view) {
+
+        OkHttpUtils<information>utils=new OkHttpUtils<>(getApplication());
+        String uru="http://101.251.196.90:8080/JztkServer/examInfo";
+        utils.url(uru).targetClass(information.class)
+            .execute(new OkHttpUtils.OnCompleteListener<information>() {
+                         @Override
+                         public void onSuccess(information result) {
+                             Log.e("main","result="+result);
+                         }
+
+                         @Override
+                         public void onError(String error) {
+                             Log.e("main","error="+error);
+
+                         }
+                     });
 
         startActivity(new Intent(MainActivity.this,ExamActivity.class));
     }
