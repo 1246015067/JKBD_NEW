0da6a93b1e15d0c31c3dd667b5194ce9fee2856d
1246015067 <1246015067@qq.com>
2017-06-30 16:56:52 +0800

    ExamApplication.java有错的

--- JKBD/app/src/main/java/com/example/ykc/jkbd/ExamApplication.java
+++ JKBD/app/src/main/java/com/example/ykc/jkbd/ExamApplication.java
@@ -3,12 +3,18 @@ package com.example.ykc.jkbd;
 import android.app.Application;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.media.MediaBrowserServiceCompat;
 import android.util.Log;
 
+import com.example.ykc.jkbd.bean.Question;
 import com.example.ykc.jkbd.bean.information;
+import com.example.ykc.jkbd.bean.reslust;
 import com.example.ykc.jkbd.utils.OkHttpUtils;
+import com.example.ykc.jkbd.utils.ResultUtils;
 
 import java.util.List;
+
+import static android.R.id.list;
 
 /**
  * Created by Administrator on 2017/6/30.
@@ -45,6 +51,10 @@ public class ExamApplication  extends Application{
         return  instance;
     }
     private void initData() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
 
         OkHttpUtils<information> utils=new OkHttpUtils<>(instance);
         String uru="http://101.251.196.90:8080/JztkServer/examInfo";
@@ -63,6 +73,31 @@ public class ExamApplication  extends Application{
                     }
                 });
 
+                OkHttpUtils<String> utils1=new OkHttpUtils<String>(instance);
+                String utils2="";
+                utils1.url(utils2);
+                utils1.targetClass(String.class);
+                utils1.execute(new OkHttpUtils.OnCompleteListener<String>() {
+
+                    @Override
+                    public void onSuccess(String result) {
+                        reslust re = ResultUtils.getListResultFromJson(result);
+                        if (re != null && re.getError_code() == 0) {
+                           List<information> list=re.getResults();
+                            if (list != null && list.size() > 0) {
+                                informationList = list;
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                            Log.e("main","error="+error);
+                    }
+                });
+
+            }
+        }).start();
 
     }
 }
