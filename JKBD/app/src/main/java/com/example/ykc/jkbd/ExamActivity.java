26d7bd9f8a6fc52bce6de0d028ec31872563f601
1246015067 <1246015067@qq.com>
2017-06-30 15:54:16 +0800

    数据显示

--- JKBD/app/src/main/java/com/example/ykc/jkbd/ExamActivity.java
+++ JKBD/app/src/main/java/com/example/ykc/jkbd/ExamActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.example.ykc.jkbd.bean.information;
 
@@ -13,12 +14,18 @@ import com.example.ykc.jkbd.bean.information;
  */
 
 public class ExamActivity extends AppCompatActivity {
+    TextView tvExamInfo;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam);
+        initView();
         initData();
 
+    }
+
+    private void initView() {
+        tvExamInfo=(TextView)findViewById(R.id.tv_exam);
     }
 
     private void initData() {
@@ -29,6 +36,6 @@ public class ExamActivity extends AppCompatActivity {
     }
 
     private void showData(information in) {
-
+        tvExamInfo.setText(in.toString());
     }
 }
